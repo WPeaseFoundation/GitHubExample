3db4be6ddaaccf44f351af5f89755997617629b4
Walter Pease <WPease@foundationsoft.com>
2019-12-19 16:12:44 -0500

    Created CoolNewClass object and GetData method.

---
+++ ConsoleApp1/ConsoleApp1/Classes/CoolNewClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Classes
+{
+    public class CoolNewClass
+    {
+        private string _data { get; set; }
+
+        public string GetData()
+        {
+            return _data;
+        }
+    }
+}
