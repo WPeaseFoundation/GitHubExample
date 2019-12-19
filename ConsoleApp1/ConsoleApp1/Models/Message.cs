b198fd862a9ce63b79efe4c05af22c6abfa9e629
Walter Pease <WPease@foundationsoft.com>
2019-12-19 16:24:08 -0500

    Created Message object and methods to access private data.

---
+++ ConsoleApp1/ConsoleApp1/Models/Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class Message
+    {
+        private string _data { get; set; }
+        private string _senderName { get; set; }
+        private DateTime _timeSent { get; set; }
+
+        public Message(string data, string senderName, DateTime timeSent)
+        {
+            _data = data;
+            _senderName = senderName;
+            _timeSent = timeSent;
+        }
+
+        public string GetData()
+        {
+            return _data;
+        }
+
+        public string GetSenderName()
+        {
+            return _senderName;
+        }
+
+        public DateTime GetTimeSent()
+        {
+            return _timeSent;
+        }
+    }
+}
